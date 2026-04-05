0c888fe5e66283f1da7fc2b2af26a272f85ebe8d
ramonmorillo <ramon.morillo.verdugo@gmail.com>
2026-04-05 10:02:57 +0200

    feat: crear MVP estático de simulador de liga

---
+++ data/names.js
@@ -0,0 +1,18 @@
+export const namePools = {
+  España: { first: ['Alejandro', 'Sergio', 'Álvaro', 'Hugo', 'Iker', 'Pablo', 'Dani', 'Javier', 'Marcos', 'Iván'], last: ['García', 'López', 'Martínez', 'Santos', 'Romero', 'Navarro', 'Gil', 'Molina', 'Ruiz', 'Torres'] },
+  Francia: { first: ['Lucas', 'Antoine', 'Mathis', 'Hugo', 'Theo', 'Nicolas'], last: ['Dubois', 'Lefevre', 'Martin', 'Bernard', 'Laurent', 'Moreau'] },
+  Alemania: { first: ['Lukas', 'Jonas', 'Maximilian', 'Felix', 'Nico', 'Leon'], last: ['Muller', 'Schmidt', 'Weber', 'Wagner', 'Fischer', 'Becker'] },
+  Italia: { first: ['Lorenzo', 'Marco', 'Davide', 'Matteo', 'Giovanni', 'Andrea'], last: ['Rossi', 'Esposito', 'Bianchi', 'Ricci', 'Romano', 'Gallo'] },
+  Portugal: { first: ['Joao', 'Diogo', 'Tiago', 'Rui', 'Goncalo', 'Pedro'], last: ['Silva', 'Santos', 'Ferreira', 'Pereira', 'Costa', 'Oliveira'] },
+  'Países Bajos': { first: ['Daan', 'Milan', 'Jesse', 'Sem', 'Ruben', 'Bram'], last: ['De Vries', 'Van Dijk', 'Bakker', 'Smit', 'Visser', 'Bos'] },
+  Bélgica: { first: ['Arthur', 'Noah', 'Louis', 'Jules', 'Milan', 'Thomas'], last: ['Peeters', 'Willems', 'Jacobs', 'Maes', 'Claes', 'Dubois'] },
+  Croacia: { first: ['Luka', 'Ivan', 'Marko', 'Ante', 'Josip', 'Petar'], last: ['Kovacic', 'Perisic', 'Novak', 'Vukovic', 'Babic', 'Maric'] },
+  Brasil: { first: ['Gabriel', 'Mateus', 'Rafael', 'Joao', 'Bruno', 'Caio'], last: ['Silva', 'Souza', 'Oliveira', 'Santos', 'Pereira', 'Costa'] },
+  Argentina: { first: ['Thiago', 'Franco', 'Agustin', 'Benjamin', 'Facundo', 'Nicolas'], last: ['Gonzalez', 'Rodriguez', 'Fernandez', 'Lopez', 'Diaz', 'Castro'] },
+  Uruguay: { first: ['Santiago', 'Nicolas', 'Martin', 'Emiliano', 'Ignacio', 'Luciano'], last: ['Pereira', 'Suarez', 'Rodriguez', 'Gomez', 'Silveira', 'Acosta'] },
+  Marruecos: { first: ['Youssef', 'Achraf', 'Sofyan', 'Amine', 'Hamza', 'Bilal'], last: ['El Amrani', 'Benali', 'Saidi', 'Alaoui', 'Bennani', 'Idrissi'] },
+  Senegal: { first: ['Moussa', 'Ibrahima', 'Cheikh', 'Mamadou', 'Pape', 'Abdou'], last: ['Ndiaye', 'Diop', 'Sarr', 'Sow', 'Ba', 'Faye'] },
+};
+
+export const euNationalities = ['España', 'Francia', 'Alemania', 'Italia', 'Portugal', 'Países Bajos', 'Bélgica', 'Croacia'];
+export const nonEuNationalities = ['Brasil', 'Argentina', 'Uruguay', 'Marruecos', 'Senegal'];
